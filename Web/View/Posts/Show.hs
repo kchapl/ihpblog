e9678671151c27c36f237487becf1aef62625866
K <kchapl@users.noreply.github.com>
2022-09-12 19:06:05 +0100

    Tutorial up to Show Comments of a Post

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -1,5 +1,6 @@
 module Web.View.Posts.Show where
 import Web.View.Prelude
+import qualified Text.MMark as MMark
 
 data ShowView = ShowView { post :: Post }
 
@@ -8,11 +9,17 @@ instance View ShowView where
         {breadcrumb}
         <h1>{get #title post}</h1>
         <p>{get #createdAt post |> timeAgo}</p>
-        <div>{get #body post}</div>
+        <div>{get #body post |> renderMarkdown}</div>
 
+        <a href={NewCommentAction (get #id post)}>Add Comment</a>
     |]
         where
             breadcrumb = renderBreadcrumb
                             [ breadcrumbLink "Posts" PostsAction
                             , breadcrumbText "Show Post"
                             ]
+
+renderMarkdown text =
+    case text |> MMark.parse "" of
+        Left error -> "Something went wrong"
+        Right markdown -> MMark.render markdown |> tshow |> preEscapedToHtml
